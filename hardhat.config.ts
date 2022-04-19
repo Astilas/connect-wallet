e094f9b67442d627154373d12ce1465d0237ee7c
Thibault Carayon <thibault@maarkt.io>
2022-04-19 09:15:01 +0200

    config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,6 +25,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    localhost: {
+      chainId: 1337,
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
